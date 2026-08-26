556e4081f400a05976c41fd4f12e4f11a1935fdf
NicolasDefelippo <nicolasdefelippo@gmail.com>
2026-08-26 19:08:19 -0300

    Update sistema_gym_grupo7.py

--- sistema_gym_grupo7.py
+++ sistema_gym_grupo7.py
@@ -4,3 +4,12 @@
 Número de socio; Nombre; Actividad principal; Valor de la cuota; Estado.
 '''
 
+ACTIVIDADES_VALIDAS = ["Musculacion", "Funcional", "Pileta", "Yoga", "Spinning"]
+
+socios = [
+    [1, "Erik Purtscher", "Musculacion", 15000, "Activo"],
+    [2, "Agostina Koya", "Pileta", 18000, "Activo"],
+    [3, "Felipe Storni", "Funcional", 16000, "Inactivo"],
+    [4, "Nicolas Defelippo", "Yoga", 14000, "Activo"],
+    [5, "Nardone Fernandez", "Spinning", 15500, "Inactivo"],
+]
