3d225f35efa3ad23b71e3a7b8e4649898df07fd5
Erik Cristopher Purtscher <epurtscher@uade.edu.ar>
2026-08-26 19:18:34 -0300

    Implement input validation for menu options
    Add input validation for menu option selection.

--- funciones/menu.py
+++ funciones/menu.py
@@ -8,5 +8,7 @@ def menu():
   print()
   print()
   print()
-  
+  op = int(input("Ingrese el númeor de una opción: "))
+  while (op <= 0 or op > 7) and op != -1:
+    print("Opción inválida - Ingrese un número del 1 al 7 (-1 para salir)")
   
