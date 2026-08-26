dd3605b4db8cd3fcdbbf5c939830c051ea446b5e
NicolasDefelippo <nicolasdefelippo@gmail.com>
2026-08-26 19:22:56 -0300

    Update menu.py

--- funciones/menu.py
+++ funciones/menu.py
@@ -1,11 +1,14 @@
-def menu():
-  print("1 - ")
-  print("2 - ")
-  print("3 - ")
-  print("4 - ")
-  print("5 - ")
-  print("6 - ")
-  print("7 - ")
+def mostrar_menu():
+    print("\n=== GESTIÓN DE GIMNASIO ===")
+    print("1. Dar de alta un socio")
+    print("2. Consultar un socio")
+    print("3. Modificar un socio")
+    print("4. Eliminar un socio")
+    print("5. Mostrar todos los socios")
+    print("6. Consultar socios por actividad")
+    print("7. Ver estadísticas")
+    print("8. Salir")
+
   op = int(input("Ingrese el númeor de una opción: "))
   while (op <= 0 or op > 7) and op != -1:
     print("Opción inválida - Ingrese un número del 1 al 7 (-1 para salir)")
