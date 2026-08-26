74849d6c0c056d5b07286af364a31bf987d691c8
NicolasDefelippo <nicolasdefelippo@gmail.com>
2026-08-26 19:44:38 -0300

    Cambios

--- funciones/crud.py
+++ funciones/crud.py
@@ -1,1 +1,12 @@
+def dar_alta():
+    None
+
+def consultar():
+    None
+
+def modificar():
+    None
+
+def eliminar():
+    None
 
--- sistema_gym_grupo7.py
+++ sistema_gym_grupo7.py
@@ -3,6 +3,8 @@
 ''' Datos de registros:
 Número de socio; Nombre; Actividad principal; Valor de la cuota; Estado.
 '''
+from funciones.menu import mostrar_menu, pedir_opcion
+
 
 ACTIVIDADES_VALIDAS = ["Musculacion", "Funcional", "Pileta", "Yoga", "Spinning"]
 
@@ -13,3 +15,26 @@ socios = [
     [4, "Nicolas Defelippo", "Yoga", 14000, "Activo"],
     [5, "Nardone Fernandez", "Spinning", 15500, "Inactivo"],
 ]
+
+mostrar_menu()
+pedir_opcion()
+
+opcion = ""
+while opcion != 8:
+    mostrar_menu()
+    opcion = pedir_opcion()
+
+    if opcion == 1:
+        None
+    elif opcion == 2:
+        None
+    elif opcion == 3:
+        None
+    elif opcion == 4:
+        None
+    elif opcion == 5:
+        None
+    elif opcion == 6:
+        None
+    elif opcion == 7:
+        None
