c4eaa91bd7a82ff0427cf4a0db9c99c60de4b7c6
NicolasDefelippo <nicolasdefelippo@gmail.com>
2026-09-05 18:20:33 -0300

    Update estadisticas.py

--- funciones/estadisticas.py
+++ funciones/estadisticas.py
@@ -5,24 +5,25 @@ def calcular_estadisticas(socios, actividades):
     total = len(socios)
     print("Cantidad total de socios:", total)
 
-    #2. Cantidad por categoría (pide una categoría puntual)
-    cont = 0
+    #2. Estadística adicional: promedio de cuota
+    suma_cuotas = 0
+    for socio in socios:
+        suma_cuotas += socio[3]
+    promedio = suma_cuotas / total
+    print("Valor promedio de cuota: $", promedio)
+    
+    #3. Cantidad por categoría (pide una categoría puntual)
+    cont = 1
     for titulos in actividades:
         print(cont, "-", titulos)
         cont += 1
     actividad = int(input("Seleccione una actividad para ver cuántos socios tiene: "))
-    while actividad >= len(actividades) or actividad < 0:
+    while actividad > len(actividades) or actividad < 1:
         actividad = int(input("Seleccione un número de actividad existente: "))
+    actividad = actividad - 1  #ajusto a índice real de la lista (arranca en 0)
 
     cantidad_actividad = 0
     for socio in socios:
         if socio[2] == actividades[actividad]:
             cantidad_actividad += 1
     print("Cantidad de socios en", actividades[actividad], ":", cantidad_actividad)
-
-    #3. Estadística adicional: promedio de cuota
-    suma_cuotas = 0
-    for socio in socios:
-        suma_cuotas += socio[3]
-    promedio = suma_cuotas / total
-    print("Valor promedio de cuota: $", promedio)
